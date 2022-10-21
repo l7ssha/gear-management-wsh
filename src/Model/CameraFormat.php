54ca19173cab87b728a12256e60c9dbef6e65d36
Szymon Uglis <szymon@l7ssha.xyz>
2022-10-21 21:22:24 +0200

    Fixup APS_C enum value

--- src/Model/CameraFormat.php
+++ src/Model/CameraFormat.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 enum CameraFormat: string
 {
-    case APS_C = 'apsc';
+    case APS_C = 'aps_c';
     case FULL_FRAME = 'full_frame';
     case HALF_FRAME = 'half_frame';
     case MEDIUM_FORMAT = 'medium_format';
