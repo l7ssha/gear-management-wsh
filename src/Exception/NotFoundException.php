180ac68a6358ea2f7f5c5a73c79d10abd2df7bc5
Szymon Uglis <szymon@l7ssha.xyz>
2022-11-06 11:55:21 +0100

    Fixup http error codes

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -3,11 +3,12 @@
 namespace App\Exception;
 
 use RuntimeException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class NotFoundException extends RuntimeException
+class NotFoundException extends HttpException
 {
     public function __construct(string $message)
     {
-        parent::__construct($message);
+        parent::__construct(404, $message);
     }
 }
